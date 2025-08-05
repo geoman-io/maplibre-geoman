3f9d22a6c43235da4516416f8cd702624bd5e381
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-08-05 23:03:49 +0200

    align PartialLayerStyle type with MapLibre Style spec

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
+        '@mapLib': path.resolve(__dirname, './src/core/map/maplibre')
       },
     },
     plugins: [
