cce628fc4cb7bbbf9e1c6c37d6cc797181065d35
Andrey M <xwild.work@gmail.com>
2025-08-27 15:12:26 +0600

    throttled/debounced updates

--- src/core/features/source-update-manager.ts
+++ src/core/features/source-update-manager.ts
@@ -1,19 +1,66 @@
 import { type FeatureSourceName, type GeoJsonSourceDiff, type Geoman, SOURCES, typedKeys } from '@/main.ts';
 import { typedValues } from '@/utils/typing.ts';
 import type { Feature } from 'geojson';
+import { debounce, throttle } from 'lodash-es';
 import log from 'loglevel';
 
+type SourceUpdateMethods = {
+  [key in FeatureSourceName]: {
+    debounced: () => void,
+    throttled: () => void,
+  }
+};
 
 export class SourceUpdateManager {
   gm: Geoman;
   updateStorage: { [key in FeatureSourceName]: Array<GeoJsonSourceDiff> };
   autoUpdatesEnabled: boolean = true;
+  delayedSourceUpdateMethods: SourceUpdateMethods;
 
   constructor(gm: Geoman) {
     this.gm = gm;
     this.updateStorage = Object.fromEntries(
       typedValues(SOURCES).map((name) => [name, []]),
     );
+
+    this.delayedSourceUpdateMethods = Object.fromEntries(
+      typedValues(SOURCES).map((sourceName) => [
+        sourceName,
+        {
+          throttled: this.getDelayedSourceUpdateMethod({
+            sourceName,
+            type: 'throttled',
+          }),
+          debounced: this.getDelayedSourceUpdateMethod({
+            sourceName,
+            type: 'debounced',
+          }),
+        } as { debounced: () => void, throttled: () => void },
+      ]),
+    ) as SourceUpdateMethods;
+  }
+
+  getDelayedSourceUpdateMethod(
+    { sourceName, type }: {
+      sourceName: FeatureSourceName,
+      type: 'throttled' | 'debounced',
+    },
+  ) {
+    if (type === 'throttled') {
+      return throttle(
+        () => this.updateSourceActual(sourceName),
+        2 * this.gm.options.settings.throttlingDelay,
+        { leading: false, trailing: true },
+      );
+    } else if (type === 'debounced') {
+      return debounce(
+        () => this.updateSourceActual(sourceName),
+        2 * this.gm.options.settings.throttlingDelay,
+        { leading: true, trailing: false },
+      );
+    } else {
+      throw new Error('Features: getDelayedSourceUpdateMethod: invalid type');
+    }
   }
 
   updateSource({ sourceName, diff }: {
@@ -24,10 +71,16 @@ export class SourceUpdateManager {
       this.updateStorage[sourceName].push(diff);
     }
 
+    this.delayedSourceUpdateMethods[sourceName].throttled();
+    this.delayedSourceUpdateMethods[sourceName].debounced();
+  }
+
+  updateSourceActual(sourceName: FeatureSourceName) {
     if (this.autoUpdatesEnabled) {
       const source = this.gm.features.sources[sourceName];
-      log.debug(`source: ${sourceName}, diffs count: ${this.updateStorage[sourceName].length}`);
+      // log.debug(`source: ${sourceName}, diffs count: ${this.updateStorage[sourceName].length}`);
       const combinedDiff = this.getCombinedDiff(sourceName);
+      // log.debug(`source: ${sourceName}, diff`, JSON.stringify(combinedDiff, null, 2));
       if (source) {
         source.updateData(combinedDiff);
       }
@@ -70,14 +123,14 @@ export class SourceUpdateManager {
 
     const nextRemoveIds = new Set(next.remove);
 
-    const pendingAdd = pending.add?.filter((f) => !nextRemoveIds.has(f.id!)) || [];
-    const pendingUpdate = pending.update?.filter((f) => !nextRemoveIds.has(f.id!)) || [];
+    const pendingAdd = pending.add?.filter((item) => !nextRemoveIds.has(item.id!)) || [];
+    const pendingUpdate = pending.update?.filter((item) => !nextRemoveIds.has(item.id!)) || [];
 
     const nextUpdate: Array<Feature> = [];
 
     next.update?.forEach((updatedFeature) => {
-      const pendingAddIdx = pendingAdd.findIndex(f => f.id === updatedFeature.id);
-      const pendingUpdateIdx = pendingUpdate.findIndex(f => f.id === updatedFeature.id);
+      const pendingAddIdx = pendingAdd.findIndex((item) => item.id === updatedFeature.id);
+      const pendingUpdateIdx = pendingUpdate.findIndex((item) => item.id === updatedFeature.id);
 
       if (pendingAddIdx === -1 && pendingUpdateIdx === -1) {
         nextUpdate.push(updatedFeature);
