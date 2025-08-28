9451a6d2e702bac4cdb48929ff1099a7881c6820
Andrey M <xwild.work@gmail.com>
2025-08-28 11:13:01 +0600

    temporary fix for playwright (SourceUpdateManager in features/index.ts)

--- src/core/features/index.ts
+++ src/core/features/index.ts
@@ -1,6 +1,5 @@
 import { gmPrefix } from '@/core/events/listeners/base.ts';
 import { FeatureData } from '@/core/features/feature-data.ts';
-import { SourceUpdateManager } from '@/core/features/source-update-manager.ts';
 import type { BaseLayer } from '@/core/map/base/layer.ts';
 import { BaseSource } from '@/core/map/base/source.ts';
 import type {
@@ -13,7 +12,7 @@ import type {
   GeoJsonImportFeature,
   GeoJsonImportFeatureCollection,
   GeoJsonShapeFeature,
-  GeoJsonShapeFeatureCollection,
+  GeoJsonShapeFeatureCollection, GeoJsonSourceDiff,
   Geoman,
   GMDrawShapeCreatedEvent,
   LngLat,
@@ -30,7 +29,7 @@ import { getGeoJsonBounds } from '@/utils/geojson.ts';
 import { isMapPointerEvent } from '@/utils/guards/map.ts';
 import { includesWithType, typedKeys, typedValues } from '@/utils/typing.ts';
 import type { Feature, FeatureCollection, GeoJSON, Geometry, LineString, MultiPolygon, Polygon } from 'geojson';
-import { cloneDeep } from 'lodash-es';
+import { cloneDeep, debounce, throttle } from 'lodash-es';
 import log from 'loglevel';
 
 
@@ -42,6 +41,163 @@ export const SOURCES: { [key: string]: string } = {
 } as const;
 
 export const FEATURE_ID_PROPERTY = '_gmid' as const;
+
+type SourceUpdateMethods = {
+  [key in FeatureSourceName]: {
+    debounced: () => void,
+    throttled: () => void,
+  }
+};
+
+// this class is here cause playwright fails if it's extracted for unknown reason
+// (possible imports trouble)
+export class SourceUpdateManager {
+  gm: Geoman;
+  updateStorage: { [key in FeatureSourceName]: Array<GeoJsonSourceDiff> };
+  autoUpdatesEnabled: boolean = true;
+  delayedSourceUpdateMethods: SourceUpdateMethods;
+
+  constructor(gm: Geoman) {
+    this.gm = gm;
+    this.updateStorage = Object.fromEntries(
+      typedValues(SOURCES).map((name) => [name, []]),
+    );
+
+    this.delayedSourceUpdateMethods = Object.fromEntries(
+      typedValues(SOURCES).map((sourceName) => [
+        sourceName,
+        {
+          throttled: this.getDelayedSourceUpdateMethod({
+            sourceName,
+            type: 'throttled',
+          }),
+          debounced: this.getDelayedSourceUpdateMethod({
+            sourceName,
+            type: 'debounced',
+          }),
+        } as { debounced: () => void, throttled: () => void },
+      ]),
+    ) as SourceUpdateMethods;
+  }
+
+  getDelayedSourceUpdateMethod(
+    { sourceName, type }: {
+      sourceName: FeatureSourceName,
+      type: 'throttled' | 'debounced',
+    },
+  ) {
+    if (type === 'throttled') {
+      return throttle(
+        () => this.updateSourceActual(sourceName),
+        this.gm.options.settings.throttlingDelay,
+        { leading: false, trailing: true },
+      );
+    } else if (type === 'debounced') {
+      return debounce(
+        () => this.updateSourceActual(sourceName),
+        this.gm.options.settings.throttlingDelay,
+        { leading: true, trailing: false },
+      );
+    } else {
+      throw new Error('Features: getDelayedSourceUpdateMethod: invalid type');
+    }
+  }
+
+  updateSource({ sourceName, diff }: {
+    sourceName: FeatureSourceName
+    diff?: GeoJsonSourceDiff,
+  }) {
+    if (diff) {
+      this.updateStorage[sourceName].push(diff);
+    }
+
+    this.delayedSourceUpdateMethods[sourceName].throttled();
+    this.delayedSourceUpdateMethods[sourceName].debounced();
+  }
+
+  updateSourceActual(sourceName: FeatureSourceName) {
+    if (this.autoUpdatesEnabled) {
+      const source = this.gm.features.sources[sourceName];
+      // log.debug(`source: ${sourceName}, diffs count: ${this.updateStorage[sourceName].length}`);
+      const combinedDiff = this.getCombinedDiff(sourceName);
+      if (source && combinedDiff) {
+        // log.debug(`source: ${sourceName}, combined diff counts`, Object.values(combinedDiff));
+        // log.debug(`source: ${sourceName}, combined diff`, JSON.stringify(combinedDiff, null, 2));
+        source.updateData(combinedDiff);
+      }
+    }
+  }
+
+  withAtomicSourcesUpdate<T>(callback: () => T): T {
+    try {
+      this.autoUpdatesEnabled = false;
+      return callback();
+    } finally {
+      typedKeys(this.gm.features.sources).forEach((sourceName) => {
+        this.updateSource({ sourceName });
+      });
+      this.autoUpdatesEnabled = true;
+    }
+  }
+
+  getCombinedDiff(sourceName: FeatureSourceName): GeoJsonSourceDiff | null {
+    let combinedDiff: GeoJsonSourceDiff = {
+      remove: [],
+      add: [],
+      update: [],
+    };
+
+    this.updateStorage[sourceName].forEach((diff) => {
+      combinedDiff = this.mergeGeoJsonDiff(combinedDiff, diff);
+    });
+
+    this.updateStorage[sourceName] = [];
+
+    if (Object.values(combinedDiff).find((item) => item.length)) {
+      return combinedDiff;
+    }
+
+    return null;
+  }
+
+  mergeGeoJsonDiff(
+    pendingDiffOrNull: GeoJsonSourceDiff | null,
+    nextDiffOrNull: GeoJsonSourceDiff | null,
+  ): GeoJsonSourceDiff {
+    const pending: GeoJsonSourceDiff = pendingDiffOrNull ?? { add: [], update: [], remove: [] };
+    const next: GeoJsonSourceDiff = nextDiffOrNull ?? { add: [], update: [], remove: [] };
+
+    const nextRemoveIds = new Set(next.remove);
+
+    const pendingAdd = pending.add?.filter((item) => !nextRemoveIds.has(item.id!)) || [];
+    const pendingUpdate = pending.update?.filter((item) => !nextRemoveIds.has(item.id!)) || [];
+
+    const nextUpdate: Array<Feature> = [];
+
+    next.update?.forEach((updatedFeature) => {
+      const pendingAddIdx = pendingAdd.findIndex((item) => item.id === updatedFeature.id);
+      const pendingUpdateIdx = pendingUpdate.findIndex((item) => item.id === updatedFeature.id);
+
+      if (pendingAddIdx === -1 && pendingUpdateIdx === -1) {
+        nextUpdate.push(updatedFeature);
+        return;
+      }
+      if (pendingAddIdx !== -1) {
+        pendingAdd[pendingAddIdx] = updatedFeature;
+      }
+      if (pendingUpdateIdx !== -1) {
+        pendingUpdate[pendingUpdateIdx] = updatedFeature;
+      }
+    });
+
+    return {
+      add: [...pendingAdd, ...next.add || []],
+      update: [...pendingUpdate, ...nextUpdate],
+      remove: [...pending.remove || [], ...next.remove || []],
+    };
+  }
+}
+
 
 export class Features {
   gm: Geoman;
