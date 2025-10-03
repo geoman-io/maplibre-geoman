80fed3ad1cf8a5657e3c0a2d551d4bab9be6e566
Andrey M <xwild.work@gmail.com>
2025-10-03 15:11:05 +0600

    event declaration import fixes

--- src/maplibre-env.d.ts
+++
@@ -1,6 +0,0 @@
-import type { EventsMap } from './types';
-
-declare module 'maplibre-gl' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  export interface MapEventType extends EventsMap {}
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
+import type { EventsMap } from '@/types/map/events-map.ts';
+
+declare module 'maplibre-gl' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface MapEventType extends EventsMap {}
+}
+
 // types
 export type * from '@/types/features.ts';
 export type * from '@/types/events/index.ts';
