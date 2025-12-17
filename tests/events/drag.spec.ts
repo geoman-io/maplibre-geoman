603ee5ec93850d30a27a98b1e640fa058532dfdf
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-12-17 20:44:19 +0100

    add extra tests for grouped features

--- tests/events/drag.spec.ts
+++ tests/events/drag.spec.ts
@@ -1,6 +1,7 @@
 import type {
   FeatureEditEndFwdEvent,
   FeatureEditStartFwdEvent,
+  FeatureId,
   FeatureUpdatedFwdEvent,
   LngLatTuple,
 } from '@/main.ts';
@@ -10,9 +11,10 @@ import centroid from '@turf/centroid';
 import { dragAndDrop, enableMode, type ScreenCoordinates } from '@tests/utils/basic.ts';
 import {
   getGeomanEventResultById,
+  saveGeomanEventResultToCustomData,
   saveGeomanFeatureEventResultToCustomData,
 } from '@tests/utils/events.ts';
-import { getRenderedFeaturesData } from '@tests/utils/features.ts';
+import { getRenderedFeaturesData, type FeatureCustomData } from '@tests/utils/features.ts';
 import { getScreenCoordinatesByLngLat } from '@tests/utils/shapes.ts';
 import { setupGeomanTest } from '@tests/utils/test-helpers.ts';
 
@@ -20,6 +22,9 @@ test.describe('Drag Events', () => {
   test.beforeEach(async ({ page }) => {
     await setupGeomanTest(page, { loadFixture: 'common-shapes' });
   });
+
+  const isNotGroupedFeature = (feature: FeatureCustomData) =>
+    typeof feature.geoJson.properties.__gm_group === 'undefined';
 
   test('should fire gm:dragstart, gm:drag, and gm:dragend events during drag operation', async ({
     page,
@@ -32,7 +37,7 @@ test.describe('Drag Events', () => {
     const features = await getRenderedFeaturesData({ page, temporary: false });
     expect(features.length).toBeGreaterThan(0);
 
-    for await (const feature of features) {
+    for await (const feature of features.filter(isNotGroupedFeature)) {
       // Get the position to drag
       let position;
       if (feature.shape === 'circle') {
@@ -59,17 +64,9 @@ test.describe('Drag Events', () => {
       const targetPoint: ScreenCoordinates = [point[0] + dX, point[1] + dY];
 
       // Set up event listeners
-      const dragStartResultId = await saveGeomanFeatureEventResultToCustomData(
-        page,
-        'dragstart',
-        feature.id,
-      );
-      const dragResultId = await saveGeomanFeatureEventResultToCustomData(page, 'drag', feature.id);
-      const dragEndResultId = await saveGeomanFeatureEventResultToCustomData(
-        page,
-        'dragend',
-        feature.id,
-      );
+      const dragStartResultId = await saveGeomanEventResultToCustomData(page, 'dragstart');
+      const dragResultId = await saveGeomanEventResultToCustomData(page, 'drag');
+      const dragEndResultId = await saveGeomanEventResultToCustomData(page, 'dragend');
 
       // Perform drag operation
       await dragAndDrop(page, point, targetPoint);
@@ -93,6 +90,102 @@ test.describe('Drag Events', () => {
       }
 
       const dragEndEvent = (await getGeomanEventResultById(page, dragEndResultId)) as
+        | FeatureEditEndFwdEvent
+        | undefined;
+      expect(dragEndEvent, 'Retrieved event result must be defined').toBeDefined();
+      if (dragEndEvent) {
+        expect(dragEndEvent.feature, 'Event feature must be defined').toBeDefined();
+        expect(dragEndEvent.shape, `Shape should be ${feature.shape}`).toEqual(feature.shape);
+      }
+    }
+  });
+
+  const isFeatureOfGroup1 = (feature: FeatureCustomData) =>
+    feature.geoJson.properties.__gm_group === 'group-1';
+
+  test('should fire gm:dragstart, gm:drag, and gm:dragend events for all grouped features', async ({
+    page,
+  }) => {
+    const dX = -30;
+    const dY = 0;
+
+    await enableMode(page, 'edit', 'drag');
+
+    const features = await getRenderedFeaturesData({ page, temporary: false });
+    expect(features.length).toBeGreaterThan(0);
+
+    const group1Features = features.filter(isFeatureOfGroup1);
+    const firstFeature = group1Features[0];
+
+    const firstFeaturePosition = getGeoJsonFirstPoint(firstFeature.geoJson);
+
+    expect(firstFeaturePosition).toBeTruthy();
+    if (!firstFeaturePosition) {
+      return;
+    }
+
+    const firstFeaturePoint = await getScreenCoordinatesByLngLat({
+      page,
+      position: firstFeaturePosition,
+    });
+
+    if (!firstFeaturePoint) {
+      return;
+    }
+
+    const firstFeatureTargetPoint: ScreenCoordinates = [
+      firstFeaturePoint[0] + dX,
+      firstFeaturePoint[1] + dY,
+    ];
+
+    const resultIdsByFeature: Map<
+      FeatureId,
+      {
+        dragStartId: string;
+        dragId: string;
+        dragEndId: string;
+      }
+    > = new Map();
+
+    for await (const feature of group1Features) {
+      // Set up event listeners
+      resultIdsByFeature.set(feature.id, {
+        dragStartId: await saveGeomanFeatureEventResultToCustomData(page, 'dragstart', feature.id),
+        dragId: await saveGeomanFeatureEventResultToCustomData(page, 'drag', feature.id),
+        dragEndId: await saveGeomanFeatureEventResultToCustomData(page, 'dragend', feature.id),
+      });
+    }
+
+    // Perform drag operation
+    await dragAndDrop(page, firstFeaturePoint, firstFeatureTargetPoint);
+
+    for await (const feature of group1Features) {
+      const resultIds = resultIdsByFeature.get(feature.id);
+      expect(resultIds).toBeDefined();
+      if (!resultIds) {
+        return;
+      }
+      const { dragStartId, dragId, dragEndId } = resultIds;
+
+      const dragStartEvent = (await getGeomanEventResultById(page, dragStartId)) as
+        | FeatureEditStartFwdEvent
+        | undefined;
+      expect(dragStartEvent, 'Retrieved event result must be defined').toBeDefined();
+      if (dragStartEvent) {
+        expect(dragStartEvent.feature, 'Event feature must be defined').toBeDefined();
+      }
+
+      const dragEvent = (await getGeomanEventResultById(page, dragId)) as
+        | FeatureUpdatedFwdEvent
+        | undefined;
+      expect(dragEvent, 'Retrieved event result must be defined').toBeDefined();
+      if (dragEvent) {
+        expect(dragEvent.feature, 'Event feature must be defined').toBeDefined();
+        expect(dragEvent.shape, `Shape should be ${feature.shape}`).toEqual(feature.shape);
+        expect(dragEvent.originalFeature, 'Event feature must be defined').toBeDefined();
+      }
+
+      const dragEndEvent = (await getGeomanEventResultById(page, dragEndId)) as
         | FeatureEditEndFwdEvent
         | undefined;
       expect(dragEndEvent, 'Retrieved event result must be defined').toBeDefined();
